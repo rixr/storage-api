1a8017e52b7d3535bdfd711c43b281b85ae07535
Ricardo <ricardoramos002@hotmail.com>
2021-07-08 00:33:12 -0700

    Se agregaron las rutas especificadas en la documentacion del proyecto.

---
+++ routes/odis-store.py
@@ -0,0 +1,29 @@
+from json import dumps as json_dumps
+from modules.auth import validate_token, auth_required
+import bottle
+
+app = bottle.Bottle()
+
+@app.post("/new")
+def store_record(*args, **kwargs):
+    bottle.response.status = 501
+    bottle.response.content_type = "application/json"
+    return dict(code= 501, message = "Not implemented")
+
+@app.get("/list")
+def get_all_info(*args, **kwargs):
+    bottle.response.status = 501
+    bottle.response.content_type = "application/json"
+    return dict(code = 501, message = "Not implemented")
+
+@app.get("/<serial_number>")
+def get_info_by_sn(*args, **kwargs):
+    bottle.response.status = 501
+    bottle.response.content_type = "application/json"
+    return dict(code = 501, message = "Not implemented")
+
+@app.post("/<serial_number>")
+def update_record(*args, code = None, **kwargs):
+    bottle.response.status = 501
+    bottle.response.content_type = "application/json"
+    return dict(code = 501, message = "Not implemented")
