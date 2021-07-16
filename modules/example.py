48f3156eaa501b04b4aaf48f221e0d22fb39959a
Miguel Alejandro Salgado Zapien <mikesalzap@gmail.com>
2021-07-15 18:32:16 -0700

    Inclui ejemplo de validacion de fecha en formato iso en modulo ejemplo

--- modules/example.py
+++ modules/example.py
@@ -8,6 +8,7 @@ NOTA:
     No debes importara ninguna cuestion relaciona con _bottle_.
 
 """
+import datetime as dt
 
 
 def example_helper_function(*args, **kwargs):
@@ -28,3 +29,36 @@ def example_helper_function(*args, **kwargs):
     print(args)
     print(kwargs)
     return None
+
+
+def estructurar_mensaje(fecha, mensaje):
+    """
+    Funcion para estructurar mensaje
+    recibe como argumentos
+     - fecha, una cadena de texto que represente la fecha
+     en formato iso.
+     - mensaje, una cadena de texto que carga el mensaje
+     a estructurar.
+
+    En caso de que el argumento no sea una fecha
+    valida en formato iso, se levantara un Excepcion
+    con mesanje `Fecha invalida.`.
+
+    Esta funcion regresara un diccionario con 2
+    llaves, `date` y `message`.
+
+    >>> estructurar_mensaje('2021-05-01T20:23:22', 'Hola mundo')
+    {'date': '2021-05-01T20:23:22', 'message': 'Hola mundo'}
+    >>> estructurar_mensaje('foo', 'Hola mundo')
+    Traceback
+     ...
+    Exception: Fecha invalida.
+    """
+    try:
+        date = dt.datetime.fromisoformat(fecha)
+    except:
+        raise Exception("Fecha invalida.")
+    return {
+        "date": date.isoformat(),
+        "message": mensaje
+    }
