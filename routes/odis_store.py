880beb83f805925efd7fb00672b6893ac232bf53
Ricardo <ricardoramos002@hotmail.com>
2021-07-24 19:33:10 -0700

    Se actualizaron las rutas del proyecto, dentro de ./routes/odis_store.py

--- routes/odis_store.py
+++ routes/odis_store.py
@@ -3,33 +3,34 @@ from json import dumps as json_dumps
 from time import time
 import bottle
 from modules.bottles import BottleJson
+from modules.auth import auth_required
+from modules.storage import store_string, get_storage_file
+from models.example import ExampleRecord
 
 app = BottleJson()
 
 @app.get("/")
-def store_record(*args, **kwargs):
-    return dict(code= 201, message = "ok")
-
 @app.post("/new")
 def store_record(*args, **kwargs):
-    payload = bottle.request.json
-    print(payload)
-    return dict(code= 201, message = "ok")
+    payload = bottle.request.query
+    print(bottle.request.query)
+    print(payload.dict)
+    raise bottle.HTTPError(501, "Error")
 
 @app.post("/<serial_number>")
 def update_record(*args, code = None, **kwargs):
     payload = bottle.request.json
     print(payload)
-    return dict(code= 201, message = "ok")
+    raise bottle.HTTPError(501, "Error")
 
 @app.get("/list")
 def get_all_info(*args, **kwargs):
-    payload = bottle.request.json
+    payload = bottle.request.query.dict
     print(payload)
-    return dict(code= 201, message = "ok")
+    raise bottle.HTTPError(501, "Error")
 
 @app.get("/<serial_number>")
-def get_info_by_sn(*args, **kwargs):
-    payload = bottle.request.json
+def get_serial_number(*args, **kwargs):
+    payload = bottle.request.query.dict
     print(payload)
-    return dict(code= 201, message = "ok")
+    raise bottle.HTTPError(501, "Error")
