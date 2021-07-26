bba6900c13ce41db1fe005093381e5e2c3fbd50f
Ricardo <ricardoramos002@hotmail.com>
2021-07-25 23:10:59 -0700

    Se agrego la validacion al primer ejemplo "/new", dentro del archivo ./routes/odis_store.py, que acepta los datos para un registro nuevo.

--- routes/odis_store.py
+++ routes/odis_store.py
@@ -6,19 +6,35 @@ from modules.bottles import BottleJson
 from modules.auth import auth_required
 from modules.storage import store_string, get_storage_file
 from models.example import ExampleRecord
+from modules.odis_store import *
 
 app = BottleJson()
 
 @app.get("/")
 @app.post("/new")
-def store_record(*args, **kwargs):
+def new(*args, **kwargs):
     payload = bottle.request.query
     print(bottle.request.query)
     print(payload.dict)
+    try:
+        brand = str(payload['brand'])
+        model = str(payload['model'])
+        serial_number = int(payload['serial_number'])
+        fecha = str(payload['date'])
+        # validación de fecha
+        year, month, day = [int(x) for x in fecha.split("-")]
+        print("Datos validos")
+        respuesta = store_register(**payload)
+    except:
+        print("Datos invalidos")
+        raise HTTPError(400)
     raise bottle.HTTPError(501, "Error")
 
+# EJEMPLO DE CURL PARA NUEVO REGISTRO
+# curl "http://localhost:8080/new?brand=getac&model=vas6150c&serial_number=1234567&date=2000-11-22"
+
 @app.post("/<serial_number>")
-def update_record(*args, code = None, **kwargs):
+def update_record(*args, **kwargs):
     payload = bottle.request.json
     print(payload)
     raise bottle.HTTPError(501, "Error")
