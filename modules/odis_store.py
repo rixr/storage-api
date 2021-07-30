3ef0214b57a9f065251f4a1432e6500109e80849
Ricardo <ricardoramos002@hotmail.com>
2021-07-30 00:29:44 -0700

    Se termino la funcion para obtener los equipos almacenados.

--- modules/odis_store.py
+++ modules/odis_store.py
@@ -1,25 +1,36 @@
 import json
-from modules.storage import store_string, store_bytes
+from modules.storage import (
+    store_string,
+    store_bytes,
+    query_storage
+)
 
-
-def get_license_to_store():
+# ESPECIFICO
+def get_license_by_sn():
     pass
 
-def get_storage_license(path=None):
-    pass
+# TODAS
+def get_storage_device(path=None):
+    query_result = query_storage(
+        "odis/device", #ruta en donde estan almacenadas las licencias
+    )
+    print(query_result)
+    return query_result['content']
+
+
 
 def store_new_device(brand=None, model=None, serial_number=None, date=None):
     filename = f"{brand}_{model}_{serial_number}.json"
     data = dict(brand=brand, model=model, serial_number=serial_number, date=date)
     store_string(
-        "odis/device",
+        "odis/device", # ruta en donde se almacenaran las licencias
         filename,
         json.dumps(data)
     )
     return f"odis/device/{filename}"
 
 def store_new_license(license_number=None, license_file=None):
-    filename = f"{license_number}.cert"
+    filename = f"{license_number}_{date}.dat"
     store_bytes(
         "odis/license",
         filename,
