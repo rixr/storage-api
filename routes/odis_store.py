2f0758d97b1574ab8f323e9107cb71fb9488c612
Ricardo <ricardoramos002@hotmail.com>
2021-08-17 17:08:25 -0700

    Se agrego breve docstring a todas las funciones.

--- routes/odis_store.py
+++ routes/odis_store.py
@@ -1,12 +1,17 @@
+import os
 import datetime as dt
 import bottle
 from modules.bottles import BottleJson
 from modules.odis_store import *
 
+# Ruta predeterminada
 app = BottleJson()
 
 @app.post("/device/new")
 def new_device(*args, **kwargs):
+    """
+    Ruta para agregar un nuevo equipo de diagnóstico
+    """
     payload = bottle.request.json
     obligatory_fields = ['brand', 'model', 'serial_number', 'date']
     try:
@@ -22,26 +27,29 @@ def new_device(*args, **kwargs):
 
 
 
-@app.post("/assign/<license_number>/<device_serial>")
-def assign_license(license_number=None, device_serial=None):
-    payload = bottle.request.json
-    obligatory_fields = ['device_serial', 'license_number']
+@app.post("/assign/<license_number>/<serial_number>")
+def assign_license(license_number, serial_number):
+    """
+    Ruta para asinar una licencia a un equipo de diagnóstico
+    """
     try:
-        if any(key not in payload for key in obligatory_fields):
-            raise Exception("Invalid data")
-        print("Valid data")
-        respuesta = assign_license2device(**payload)
+        respuesta = assign_license2device()
     except:
         print("Invalid data")
-        raise bottle.HTTPError(400)
+        raise bottle.HTTPError(400, "MALA RESPUESTA")
     raise bottle.HTTPError(201, respuesta)
 
 
 
 @app.post("/license/new/<license_number>")
 def new_license(license_number):
+    """
+    Ruta para agregar una nueva licencia
+    """
     try:
         license_file = bottle.request.files.get("license_file")
+        license_size = os.stat(license_file).st_size()
+        print(license_size)
         payload = {
             "license_number": license_number,
             "license_file": license_file.file
@@ -57,6 +65,9 @@ def new_license(license_number):
 
 @app.get("/list")
 def get_all_devices(*args, **kwargs):
+    """
+    Ruta para consultar todos los equipos registrados
+    """
     try:
         respuesta = get_storage_device()
     except:
@@ -66,6 +77,9 @@ def get_all_devices(*args, **kwargs):
 
 @app.get("/device/<serial_number>")
 def license_per_sn(*args, code=None, **kwargs):
+    """
+    Ruta para consultar equipos registrados dado su numero de serie
+    """
     try:
         respuesta = get_license_by_sn(serial_number=code)
     except:
